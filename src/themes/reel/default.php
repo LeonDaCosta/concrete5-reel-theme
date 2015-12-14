ff14ea59d9087df33f19f9b8692803afb33163d3
Leon Da Costa <leon@dacosta.me>
2015-12-14 23:30:05 +0000

    Setting up package 0.0.2

---
+++ src/themes/reel/default.php
@@ -0,0 +1,15 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+$this->inc('elements/header.php'); ?>
+<main>
+<?php
+$a = new Area('Main');
+$a->enableGridContainer();
+$a->display($c);
+
+$a = new Area('Page Footer');
+$a->enableGridContainer();
+$a->display($c);
+?>
+</main>
+<?php  $this->inc('elements/footer.php'); ?>
---
+++ src/themes/reel/elements/footer_bottom.php
@@ -0,0 +1,5 @@
+<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+</div>
+<?php Loader::element('footer_required'); ?>
+</body>
+</html>
