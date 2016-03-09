4fca51a9288d1d35c4864523cc082748e98bec02
Leon Da Costa <leon@onbrandgroup.co.uk>
2016-03-09 13:13:40 +0000

    Addition function add to gulp file

--- gulp.js
+++ gulp.js
@@ -3,3 +3,7 @@ var gulp = require('gulp');
 gulp.task('default', function() {
   // place code for your default task here
 });
+
+gulp.task('production', function() {
+  // place code for your default task here
+});
