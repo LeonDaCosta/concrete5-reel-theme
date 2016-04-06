116a259426524ad468c9d26d28ce0341a82b13e8
Leon Da Costa <dacosta.ldc@gmail.com>
2016-04-06 22:54:33 +0100

    Create reel_nav.php

---
+++ src/blocks/autonav/templates/reel_nav.php
@@ -0,0 +1,82 @@
+<?php defined('C5_EXECUTE') or die("Access Denied."); 
+
+php View::getInstance()->requireAsset('javascript', 'jquery');
+
+$navItems = $controller->getNavItems();
+
+/*** STEP 1 of 2: Determine all CSS classes (only 2 are enabled by default, but you can un-comment other ones or add your own) ***/
+foreach ($navItems as $ni) {
+    $classes = array();
+
+    if ($ni->isCurrent) {
+        //class for the page currently being viewed
+        $classes[] = 'nav-selected';
+    }
+
+    if ($ni->inPath) {
+        //class for parent items of the page currently being viewed
+        $classes[] = 'nav-path-selected';
+    }
+
+    /*
+    if ($ni->isFirst) {
+        //class for the first item in each menu section (first top-level item, and first item of each dropdown sub-menu)
+        $classes[] = 'nav-first';
+    }
+    */
+
+    /*
+    if ($ni->isLast) {
+        //class for the last item in each menu section (last top-level item, and last item of each dropdown sub-menu)
+        $classes[] = 'nav-last';
+    }
+    */
+
+    /*
+    if ($ni->hasSubmenu) {
+        //class for items that have dropdown sub-menus
+        $classes[] = 'nav-dropdown';
+    }
+    */
+
+    /*
+    if (!empty($ni->attrClass)) {
+        //class that can be set by end-user via the 'nav_item_class' custom page attribute
+        $classes[] = $ni->attrClass;
+    }
+    */
+
+    /*
+    if ($ni->isHome) {
+        //home page
+        $classes[] = 'nav-home';
+    }
+    */
+
+    /*
+    //unique class for every single menu item
+    $classes[] = 'nav-item-' . $ni->cID;
+    */
+
+    //Put all classes together into one space-separated string
+    $ni->classes = implode(" ", $classes);
+}
+
+echo '<nav class="ccm-responsive-navigation original"><ul>'; //opens the top-level menu
+
+foreach ($navItems as $ni) {
+
+    echo '<li class="' . $ni->classes . '">'; //opens a nav item
+    $name = (isset($translate) && $translate == true) ? t($ni->name) : $ni->name;
+    echo '<a href="' . $ni->url . '" target="' . $ni->target . '" class="' . $ni->classes . '">' . $name . '</a>';
+
+    if ($ni->hasSubmenu) {
+        echo '<ul>'; //opens a dropdown sub-menu
+    } else {
+        echo '</li>'; //closes a nav item
+        echo str_repeat('</ul></li>', $ni->subDepth); //closes dropdown sub-menu(s) and their top-level nav item(s)
+    }
+}
+
+echo '</ul></nav>'; //closes the top-level menu
+echo '<div class="ccm-responsive-menu-launch"><i></i></div>'; // empty i tag for attaching :after or :before psuedos for things like FontAwesome icons.
