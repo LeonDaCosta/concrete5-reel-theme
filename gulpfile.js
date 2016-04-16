db1d94ecafbe5033642b805bc0b91b727aba91b8
Leon Da Costa <dacosta.ldc@gmail.com>
2016-04-16 23:01:28 +0100

    Setting up packets 0.0.12

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 
-
-gulp.task('default', ['sass:watch','styles','script']);
+gulp.task('default', ['styles','script']);
 
 gulp.task('production', ['styles']);
 
@@ -19,21 +17,4 @@ gulp.task('compress', function() {
   return gulp.src('src/themes/reel/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('build/themes/reel/js'));
-});
-
-gulp.task('sass', function () {
-  return gulp.src('src/themes/reel/sass/**/*.sass')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('src/themes/reel/css'));
-});
-
-gulp.task('scss', function () {
-  return gulp.src('src/themes/reel/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('src/themes/reel/css'));
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('src/themes/reel/sass/**/*.sass', ['sass']);
-  gulp.watch('src/themes/reel/scss/**/*.scss', ['scss']);
 });
