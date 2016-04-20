fc6d9d536a3d84ef1caee3371f0c2d998be75341
Leon Da Costa <leon@dacosta.me>
2016-04-20 23:34:42 +0100

    Setting up packets 0.0.14

--- src/themes/reel/elements/footer.php
+++ src/themes/reel/elements/footer.php
@@ -1,15 +1,13 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");
 
-$footerSiteTitle = new GlobalArea('Footer Site Title');
-$footerSocial = new GlobalArea('Footer Social');
-$footerSiteTitleBlocks = $footerSiteTitle->getTotalBlocksInArea();
-$footerSocialBlocks = $footerSocial->getTotalBlocksInArea();
-$displayFirstSection = $footerSiteTitleBlocks > 0 || $footerSocialBlocks > 0 || $c->isEditMode();
+//$footerSiteTitle = new GlobalArea('Footer Site Title');
+//$footerSocial = new GlobalArea('Footer Social');
+//$footerSiteTitleBlocks = $footerSiteTitle->getTotalBlocksInArea();
+//$footerSocialBlocks = $footerSocial->getTotalBlocksInArea();
+//$displayFirstSection = $footerSiteTitleBlocks > 0 || $footerSocialBlocks > 0 || $c->isEditMode();
 
 ?>
-
 <footer id="footer-theme">
-    <?php if ($displayFirstSection) { ?>
     <section>
     <div class="container">
         <div class="row">
@@ -28,7 +26,7 @@ $displayFirstSection = $footerSiteTitleBlocks > 0 || $footerSocialBlocks > 0 || 
         </div>
     </div>
     </section>
-    <?php } ?>
+
     <section>
     <div class="container">
         <div class="row">
@@ -66,6 +64,5 @@ $displayFirstSection = $footerSiteTitleBlocks > 0 || $footerSocialBlocks > 0 || 
         </div>
     </div>
 </footer>
-
 
 <?php $this->inc('elements/footer_bottom.php');?>
